2ee4a1fb46e338cbe5da62deb6e02ff82e4d7152
Prashant Bhalesain <pbhalesain@gmail.com>
2016-10-09 15:50:11 +0100

    rails_apps_composer: add README files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  def after_sign_in_path_for(resource)
+    case current_user.role
+      when 'admin'
+        users_path
+      when 'silver'
+        content_silver_path
+      when 'gold'
+        content_gold_path
+      when 'platinum'
+        content_platinum_path
+      else
+        root_path
+    end
   end
 
 end
